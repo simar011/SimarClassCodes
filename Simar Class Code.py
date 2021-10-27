f64558a2220841a137cd20bf81bf52ab49b39396
simar011 <93173580+simar011@users.noreply.github.com>
2021-10-27 16:11:55 +0530

    Add files via upload

---
+++ Simar Class Code.py
@@ -0,0 +1,24 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #KidZone Project
+>>> age = input("Enter the Kid age : ")
+Enter the Kid age : 10
+>>> print(age)
+10
+>>> print(type(age))
+<class 'str'>
+>>> print("Convert the string to number use int() method")
+Convert the string to number use int() method
+>>> age_in_text= int(age)
+>>> print(age_in_text)
+10
+>>> print(type(age_in_text))
+<class 'int'>
+>>> print("Convert the number to string use str() method")
+Convert the number to string use str() method
+>>> age_in_number= str(age_in_text)
+>>> print(age_in_text)
+10
+>>> print(type(age_in_text))
+<class 'int'>
+>>> 
